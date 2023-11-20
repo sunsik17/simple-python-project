252973aa800866a118ab24af54661024cfbf3921
sunsik <sunsig21c@gmail.com>
2023-11-20 13:29:55 +0900

    textwrap, re, collections 라이브러리 사용
    textwrap = 문자열을 다룬다. (자르기, 줄바꿈 등)
    re = 문자열 정규표현식 사용
    collections 에서 counter = 특정 문자열이 몇번 나오는지 확인가능

--- textwrap.py
+++ textwrap.py
@@ -1,4 +1,6 @@
 import textwrap3
+import re
+import collections
 
 txt = '최근 일부 프로젝트가 프론트 엔드에서 파이썬을 자바스크립트로 트랜스파일하거나 웹어셈블리를 통해 프런트엔드에서 파이썬을 실행하는 방법을 시도했지만, 현재 구현되는 방식은 투박하고 원시적이다. 개발자에게 지금 바로 더 좋은 선택지는 없을까? 물론 있다. 새로운 파이썬 웹 프레임워크 제품군을 사용하면 백엔드에서 프로그래밍 방식으로 프런트 엔드 코드를 생성하는 선언적인 파이썬 코드를 작성할 수 있다. 파이썬 객체를 사용해 HTML 엔티티와 해당 자바스크립트 기반 동작을 기술한 다음, 클라이언트에 제공될 때 프레임워크가 이러한 객체를 자동으로 생성하도록 할 수 있다.'
 print(txt)
@@ -11,3 +13,10 @@ print(wrapped_txt)
 print('\n'.join(wrapped_txt))
 filled_txt = textwrap3.fill(txt, width=40)
 print(filled_txt)
+
+txt = textwrap3.fill(txt, width=50)
+words = re.findall(r'\w+', txt)
+print(words)
+
+counter = collections.Counter(words)
+print(counter)
