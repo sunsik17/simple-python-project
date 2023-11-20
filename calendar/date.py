6a55b122a8fcd49833268827868c1317c37cc661
sunsik <sunsig21c@gmail.com>
2023-11-20 14:46:41 +0900

    dateutil 라이브러리, time 모듈
    dateutil.parse는 문자열형식의 날짜를 판단해 datetime객체로 변환해 줌
    log = 'INFO 2022-01-01T00:00:01 Happy new year, human.'
    와 같은 문자열에서 날짜형식을 읽어오려면
    fuzzy=True 인자가 필요함
    
    time = 보통 경과시간을 알려고 할 때 사용

--- calendar/date.py
+++ calendar/date.py
@@ -19,4 +19,46 @@ print(calendar.isleap(2022))
 
 print(calendar.leapdays(1990, 2022))
 print(calendar.weekday(2023, 11, 20))
-print(calendar.calendar(2023))
+# print(calendar.calendar(2023))
+
+tmpTime_str = '2023-11-08 16:04:30'
+tmpTime = datetime.datetime.strptime(tmpTime_str, '%Y-%m-%d %H:%M:%S')
+print(type(tmpTime))
+
+now = datetime.datetime.now()
+print(now)
+now_str = now.strftime('%Y-%m-%d')
+print(type(now_str))
+print(now_str)
+now_str2 = now.strftime('%H:%M:%S')
+print(type(now_str2))
+print(now_str2)
+
+from dateutil.parser import parse
+
+date = '2023-11-20'
+parsed = parse(date)
+print(parsed)
+print(parse("Oct 15, 2022 04:05:32 PM"))
+
+log = 'INFO 2022-01-01T00:00:01 Happy new year, human.'
+print(parse(log, fuzzy=True))
+
+import time
+
+print(time.time())
+print(time.ctime())
+
+print('1초 대기 시작')
+time.sleep(1)
+print('대기 끝')
+
+start = time.time()
+
+for i in range(5):
+    time.sleep(1)
+    print('반복 회수', i + 1)
+
+end = time.time()
+
+print('경과 시간은 {}초 입니다'.format(end - start))
