4bd319e2cb22f2aaa2d667e1cb103d21442c51b3
sunsik <sunsig21c@gmail.com>
2023-11-20 16:56:51 +0900

    logging 모듈 사용 log 남기기
    로그 레벨 : debug < info < warning < error < critical
    
    핸들러나 로거의 로그레벨을 info로 설정하면 info이상의 레벨 로그만 출력
    debug는 출력 x

---
+++ crawling-tutorial/logging_tutorial.py
@@ -0,0 +1,17 @@
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+formatting = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatting)
+logger.addHandler(stream_handler)
+
+file_handler = logging.FileHandler('sample.log')
+file_handler.setFormatter(formatting)
+logger.addHandler(file_handler)
+
+def myFunc():
+    logger.info('실행 로그')
+
+myFunc()
