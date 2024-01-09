f2c5db4c5297b6b55b0f2d0f3e74d0ef3b002c17
sunsik17 <sunsig21c@gmail.com>
2024-01-09 13:26:20 +0900

    크롤링 연습 - 2
    pythonic하게 코드 리팩터링
    python - 컴프리헨션
    select, find method 변경

--- crawling_demo/crawling_demo.py
+++ crawling_demo/crawling_demo.py
@@ -15,16 +15,16 @@ singer_url = "https://www.melon.com/artist/timeline.htm?artistId="
 album_url = "https://www.melon.com/album/detail.htm?albumId="
 rank = 1
 for i in result_chart:
-    print('%02d' % rank, end="위 \n 제목: ")
     song_title = i.select_one(".ellipsis.rank01 a").text
-    print(song_title, end="\n 가수: ")
     singer = i.select_one(".ellipsis.rank02 a")
-    print(singer.text, end="\n 가수 정보 링크: ")
     singer_id = get_id(singer["href"])
-    print(f"{singer_url + singer_id}", end="\n 앨범: ")
     album = i.select_one(".ellipsis.rank03 a")
-    print(album.text, end="\n 앨범 정보 링크: ")
     album_id = get_id(album["href"])
+    print('%02d' % rank, end="위 \n 제목: ")
+    print(song_title, end="\n 가수: ")
+    print(singer.text, end="\n 가수 정보 링크: ")
+    print(f"{singer_url + singer_id}", end="\n 앨범: ")
+    print(album.text, end="\n 앨범 정보 링크: ")
     print(f"{album_url + album_id}")
     rank += 1
     
